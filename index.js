8e4634f61c5adf2b726b918a457846f09afb2830
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2018-02-21 23:55:13 -0500

    add: movement to ship

--- index.js
+++ index.js
@@ -16,11 +16,23 @@ function create() {
   player = game.add.sprite(400,500,'ship');
   //position the player after adding it
   player.anchor.setTo(0.5,0.5);
+  game.physics.enable(player, Phaser.Physics.ARCADE);
+  cursors= game.input.keyboard.createCursorKeys();
 }
 //the update function will add movement
 function update() {
-//adding the y axis of the tile position will scroll the background up so if i change to x i can scroll the background right and this inpsires a mario runner game 
+//adding the y axis of the tile position will scroll the background up so if i change to x i can scroll the background right and this inpsires a mario runner game
   starfield.tilePosition.y +=2;
+
+  player.body.velocity.setTo(0,0);
+
+  if(cursors.left.isDown) {
+    //manipulating along the x axis here will move the player left and right
+    player.body.velocity.x=-200;
+  }
+  else if (cursors.right.isDown){
+    player.body.velocity.x=200;
+  }
 }
 
 function render() {
